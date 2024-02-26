f78eb939aae83050a2cd6b7e0b069c2f8ca8b798
Isabel <normarojasg@gmail.com>
2024-02-26 10:53:04 -0300

    últimas actualizaciones al código JS y algunos ajustes al HTML

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-
-$(document).ready(function(){
-    //código jQuery adicional
-  $(".navbar a").on('click', function(event) {
-    if (this.hash !== "") {
-    event.preventDefault();
-    var hash = this.hash;
-    $('html, body').animate({
-    scrollTop: $(hash).offset().top
-    }, 800, function(){
-   
-    window.location.hash = hash;
-    });
-    }
-    });
-   });
-
-   $("#botonAlerta").click(function(){
-    alert("El correo fue enviado correctamente")
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
 })
 
+$(document).ready(function () {
+    //código jQuery adicional
 
+    $(".navbar a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    })
+
+    $("#botonAlerta").click(function () {
+        alert("El correo fue enviado correctamente")
+    })
+
+    $("#mititulo").on({
+        mouseover: function () {
+            $(this).css("font-size", "30px", "font-weight-bold");
+        },
+        mouseout: function () {
+            $(this).css("font-size", "20px");
+        }
+    })
+})
